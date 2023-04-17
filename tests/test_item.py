471ab1c45dd370bffd73b1c2da17afe3492d9af5
Kirbenev <kirbenev@bk.ru>
2023-04-17 21:37:11 +0300

    homework 3

--- tests/test_item.py
+++ tests/test_item.py
@@ -24,3 +24,9 @@ def test_instantiate_from_csv():
 def test_string_to_number():
     assert Item.string_to_number('10') == 10
     assert Item.string_to_number('11.789') == 11
+
+def test_item_repr(test_item):
+    assert repr(test_item) == "Item('Смартфон', 10000, 20)"
+
+def test_item_str(test_item):
+    assert str(test_item) == 'Смартфон'
